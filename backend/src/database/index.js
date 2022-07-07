b8fc07696c2ab17306c1df6c7d52ffa761a7a5d0
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-07-07 13:53:52 +0200

    feat: use local db

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -8,16 +8,17 @@ const DB_URL =
             env === "development" ? process.env.DB_URL_DEV :
                 process.env.DB_URL_DEV;
 
-const db = mongoose.connect("mongodb+srv://sandberg-g:jLx6i4RujUt84k4@cluster0.mjmat.mongodb.net/mern-auth?retryWrites=true&w=majority", {
+const db = mongoose
+  .connect("mongodb://localhost:27017/vehicle_mis", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-    .then((connection) => {
-        console.log("Connected to MongoDB Successful");
-    })
-    .catch(err => {
-        console.log("Database url: " + DB_URL);
-        console.log(err);
-    })
+  })
+  .then((connection) => {
+    console.log("Connected to MongoDB Successful");
+  })
+  .catch((err) => {
+    console.log("Database url: " + DB_URL);
+    console.log(err);
+  });
 
 export default db;
